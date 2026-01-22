b8b1d14181c20c9a941d735291cbd7a1b8b80923
Admin <admin@hospital.com>
2026-01-22 07:54:15 +0700

    fix for render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// การตั้งค่านี้จะช่วยให้ Render รู้จักไลบรารี Excel (xlsx) โดยไม่ Error
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    include: ['xlsx'],
+  },
 })
